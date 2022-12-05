f1cd6fcb17fb8d920af76df2ab8f2f29cb4285aa
4Dark5Helmet1 <114078372+4Dark5Helmet1@users.noreply.github.com>
2022-12-05 17:34:56 +0800

    1
    Orlov

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vot/models/Cart.dart';
+import 'package:vot/models/product.dart';
+import 'pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ProductDataProvider>(
+          create: (context) => ProductDataProvider(),
+        ),
+        ChangeNotifierProvider<CartDataProcider>(
+          create: (context) => CartDataProcider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Demo',
+        theme: ThemeData(
+          primaryColor: Colors.amber,
+          backgroundColor: Colors.white,
+          textTheme: const TextTheme(headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),),
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
